9d5ecc8fe87ddb13db1fd59837bf235f8e0e2e9c
Lukasz Kurek <lukascoorek@gmail.com>
2019-05-20 12:06:40 +0200

    ico icon editing

--- src/modules/forecast.js
+++ src/modules/forecast.js
@@ -1,6 +1,6 @@
 export default class Forecast {
   constructor() {
-    this.key = "8qB5DiGAaxRdrFZy1kiMAiWGoHKhlAEK";
+    this.key = "83OIsCZKittU4uKDOh7vbsk3aNPGdivQ";
     this.baseURI =
       "https://dataservice.accuweather.com/locations/v1/cities/search";
     this.baseCondURI =
--- webpack.config.js
+++ webpack.config.js
@@ -27,21 +27,21 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
-      },
-      {
-        test: /\.html$/,
-        use: ["html-loader"]
-      },
-      {
-        test: /\.(svg|png|jpg|gif|ico)$/,
-        use: {
-          loader: "file-loader",
-          options: {
-            name: "[name].[hash].[ext]",
-            outputPath: "imgs"
-          }
-        }
       }
+      // {
+      //   test: /\.html$/,
+      //   use: ["html-loader"]
+      // },
+      // {
+      //   test: /\.(svg|png|jpg|gif|ico)$/,
+      //   use: {
+      //     loader: "file-loader",
+      //     options: {
+      //       name: "[name].[hash].[ext]",
+      //       outputPath: "imgs"
+      //     }
+      //   }
+      // }
     ]
   }
 };
