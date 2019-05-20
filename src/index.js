32e5749e7bb5087fd351548e12a02a5634c755cb
Lukasz Kurek <lukascoorek@gmail.com>
2019-05-20 10:22:11 +0200

    ui working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
 import "../src/styles/main.scss";
 import "@babel/polyfill";
 import Forecast from "../src/modules/forecast";
-
 const forecast = new Forecast();
+const form = document.querySelector("form");
+const cityDets = document.querySelector(".card_details");
+const card = document.querySelector(".card");
+const cardImg = document.querySelector(".card img");
+const cardIcon = document.querySelector(".icon img");
+
+const updateUI = ({ location, conditions }) => {
+  const imgSrc = conditions.IsDayTime
+    ? "../src/assets/img/day.svg"
+    : "../src/assets/img/night.svg";
+  cardImg.setAttribute("src", imgSrc);
+
+  const iconSrc = `../src/assets/img/icons/${conditions.WeatherIcon}.svg`;
+  cardIcon.setAttribute("src", iconSrc);
+
+  cityDets.innerHTML = `
+    <h5 class="my-3">${location.EnglishName}</h5>
+    <div class="my-3">${conditions.WeatherText}</div>
+    <div class="display-4 my-4"><span>${
+      conditions.Temperature.Metric.Value
+    }</span><span>&deg;C</span></div>
+  `;
+  card.classList.remove("d-none");
+};
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const city = form.city.value.trim();
+  form.reset();
+  forecast
+    .updateCity(city)
+    .then(data => updateUI(data))
+    .catch(error => console.log(error));
+  localStorage.setItem("city", city);
+});
+
+const city = localStorage.getItem("city");
+if (city) {
+  forecast
+    .updateCity(city)
+    .then(data => updateUI(data))
+    .catch(error => console.log(error));
+}
--- src/modules/forecast.js
+++ src/modules/forecast.js
@@ -3,11 +3,30 @@ export default class Forecast {
     this.key = "XMKlXO38GCuOsGonhqG3wSrL6MTCScQr";
     this.baseURI =
       "http://dataservice.accuweather.com/locations/v1/cities/search";
+    this.baseCondURI =
+      "http://dataservice.accuweather.com/currentconditions/v1/";
   }
   async getLocation(city) {
     const query = `?apikey=${this.key}&q=${city}`;
     const response = await fetch(this.baseURI + query);
+    if (response.status !== 200) {
+      throw new Error("cannot fetch data");
+    }
     const data = await response.json();
-    console.log(data);
+    return data[0];
+  }
+  async getConditions(cityKey) {
+    const query = `${cityKey}?apikey=${this.key}`;
+    const response = await fetch(this.baseCondURI + query);
+    if (response.status !== 200) {
+      throw new Error("cannot fetch data");
+    }
+    const data = await response.json();
+    return data[0];
+  }
+  async updateCity(city) {
+    const location = await this.getLocation(city);
+    const conditions = await this.getConditions(location.Key);
+    return { location, conditions };
   }
 }
